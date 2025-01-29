e5cdb0ba21537ae2976e3600179b5daef9ba5960
Alex Neuro <alex.neu.ctmass@gmail.com>
2025-01-29 23:46:15 +0300

    Create component for mini preview specialist

--- src/pages/dashboard/specialist/agreedProfile/ConnectionsAndFriend.js
+++ src/pages/dashboard/specialist/agreedProfile/ConnectionsAndFriend.js
@@ -2,6 +2,8 @@ import {Avatar, Box, Grid, Link, Typography} from "@mui/material";
 import React from "react";
 import {RouterLink} from "src/components/router-link";
 import {paths} from "src/paths";
+import {SpecialistMiniPreview} from "src/sections/components/specialist/specialist-mini-preview";
+
 
 export default function ConnectionsAndFriend(props) {
     const {friends} = props;
@@ -14,56 +16,13 @@ export default function ConnectionsAndFriend(props) {
                     <Link
                         component={RouterLink}
                         href={friend.link}
-                        sx={{display: "contents", color:"inherit"}}
+                        sx={{display: "contents", color: "inherit"}}
                     >
-                        <Grid container spacing={3} alignItems="flex-start">
-                            {/* Аватар */}
-                            <Grid item>
-                                <Avatar sx={{width: 110, height: 130, borderRadius: 2}} variant="square"
-                                        src={friend.avatar}/>
-                            </Grid>
-
-                            {/* Информация о профиле */}
-                            <Grid item xs>
-                                <Box display="flex" flexDirection="column" alignItems="flex-start">
-                                    {/* Имя и значок сертификации */}
-                                    <Box display="flex" alignItems="flex-start">
-                                        <Typography fontWeight="bold" variant="h7">
-                                            {friend.name}
-                                        </Typography>
-                                    </Box>
-                                </Box>
-                                <Typography variant="h8" color="text.secondary">{friend.specName}</Typography>
-
-                                {/* Рейтинг */}
-                                <Box display="flex" marginTop="10px" alignItems="center">
-                                    <Box
-                                        component="img"
-                                        src="/star.png"
-                                        alt="Rating"
-                                        sx={{height: 20, mr: 1}}
-                                    />
-                                    <Typography color="text.secondary" sx={{whiteSpace: 'pre-wrap', fontSize: 12}}>
-                                        {friend.rating} · {friend.reviewsCount} reviews
-                                    </Typography>
-                                </Box>
-
-                                {/* Локация */}
-                                <Box display="flex" marginTop="5px">
-                                    <Box
-                                        component="img"
-                                        src="/place.png"
-                                        alt="Location"
-                                        sx={{height: 20, mr: 1}}
-                                    />
-                                    <Typography color="text.secondary"
-                                                sx={{fontSize: 12}}>{friend.location}</Typography>
-                                </Box>
-                            </Grid>
-                        </Grid>
+                        <SpecialistMiniPreview specialist={friend}/>
                     </Link>
                 ))
-                }</Box>
+                }
+            </Box>
         </div>
     );
 }
---
+++ src/sections/components/specialist/specialist-mini-preview.js
@@ -0,0 +1,60 @@
+import {Avatar, Box, Grid, Typography} from "@mui/material";
+import PropTypes from "prop-types";
+import React from "react";
+import {TopNav} from "src/layouts/marketing/top-nav";
+
+export const SpecialistMiniPreview = (props) => {
+    const {specialist} = props;
+
+
+    return <Grid container spacing={3} alignItems="flex-start">
+        {/* Аватар */}
+        <Grid item>
+            <Avatar sx={{width: 110, height: 130, borderRadius: 2}} variant="square"
+                    src={specialist.avatar}/>
+        </Grid>
+
+        {/* Информация о профиле */}
+        <Grid item xs>
+            <Box display="flex" flexDirection="column" alignItems="flex-start">
+                {/* Имя и значок сертификации */}
+                <Box display="flex" alignItems="flex-start">
+                    <Typography fontWeight="bold" variant="h7">
+                        {specialist.name}
+                    </Typography>
+                </Box>
+            </Box>
+            <Typography variant="h8" color="text.secondary">{specialist.specName}</Typography>
+
+            {/* Рейтинг */}
+            <Box display="flex" marginTop="10px" alignItems="center">
+                <Box
+                    component="img"
+                    src="/star.png"
+                    alt="Rating"
+                    sx={{height: 20, mr: 1}}
+                />
+                <Typography color="text.secondary" sx={{whiteSpace: "pre-wrap", fontSize: 12}}>
+                    {specialist.rating} · {specialist.reviewsCount} reviews
+                </Typography>
+            </Box>
+
+            {/* Локация */}
+            <Box display="flex" marginTop="5px">
+                <Box
+                    component="img"
+                    src="/place.png"
+                    alt="Location"
+                    sx={{height: 20, mr: 1}}
+                />
+                <Typography color="text.secondary"
+                            sx={{fontSize: 12}}>{specialist.location}</Typography>
+            </Box>
+        </Grid>
+    </Grid>;
+}
+
+
+SpecialistMiniPreview.propTypes = {
+    specialist: PropTypes.object.isRequired
+};
