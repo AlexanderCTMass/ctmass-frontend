ab581bba14f1cd9aa2f8a545a886b3bbe123f786
Alex Neuro <alex.neu.ctmass@gmail.com>
2025-05-05 18:41:29 +0300

    Добавил новые иконки для платежных систем - 10 минут

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,14 @@
-import { onCall } from 'firebase-functions/v2/https'
-import { PaymentService } from './src/services/payment/payment.service.js'
-import { updateDonationRank } from './src/triggers/donations/donation.triggers.js'
-import { HttpsError } from 'firebase-functions/v2/https'
+import {HttpsError, onCall} from 'firebase-functions/v2/https'
+import {PaymentService} from './src/services/payment/payment.service.js'
 
 export const createStripePaymentIntent = onCall(
-    { enforceAppCheck: false, cors: true },
+    {region: "us-central1", enforceAppCheck: false, cors: true},
     async (request) => {
         if (!request.auth) {
             throw new HttpsError('unauthenticated', 'Authentication required')
         }
 
-        const { amount, currency = 'usd' } = request.data
+        const {amount, currency = 'usd'} = request.data
         return PaymentService.createPaymentIntent(amount, currency, request.auth.uid)
     }
 )
-
-export { updateDonationRank }
