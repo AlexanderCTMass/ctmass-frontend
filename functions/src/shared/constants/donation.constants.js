5f91e4040a93ed2da7b76142ca1e08d3207fed46
Alex Neuro <alex.neu.ctmass@gmail.com>
2026-03-02 01:55:41 +0300

    Add post-newsletters

--- functions/src/shared/constants/donation.constants.js
+++ functions/src/shared/constants/donation.constants.js
@@ -1,7 +1,7 @@
 export const DONATION_TIERS = [
-    { amount: 1000, badge: "Platinum Sponsor" },
-    { amount: 500, badge: "Gold Supporter" },
-    { amount: 100, badge: "Silver Backer" },
-    { amount: 50, badge: "Bronze Contributor" },
-    { amount: 10, badge: "Supporter" }
-]
+  { amount: 1000, badge: "Platinum Sponsor" },
+  { amount: 500, badge: "Gold Supporter" },
+  { amount: 100, badge: "Silver Backer" },
+  { amount: 50, badge: "Bronze Contributor" },
+  { amount: 10, badge: "Supporter" },
+];
