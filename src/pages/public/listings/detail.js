e49d53617ae146e6ee1a64cf02c4faa181f51288
George Deriugin <george.jackboys@gmail.com>
2026-06-14 02:39:03 +0300

    feat: updated mobile adaptation for blogs and listings

--- src/pages/public/listings/detail.js
+++ src/pages/public/listings/detail.js
@@ -597,7 +597,7 @@ const Page = () => {
     return (
         <>
             <Seo title={listing.title}/>
-            <Box component="main" sx={{flexGrow: 1, py: 8}}>
+            <Box component="main" sx={{flexGrow: 1, pt: {xs: '94px', md: '110px'}, pb: 8}}>
                 <Container maxWidth="xl">
                     {/* Навигация */}
                     <Stack direction="row" alignItems="center" spacing={2} sx={{mb: 4}}>
